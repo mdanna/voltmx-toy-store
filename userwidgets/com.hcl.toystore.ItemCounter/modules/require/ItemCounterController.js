abd3a9e714e8ba83aa2f01a4b6c51af827779091
Mario D'Anna <mario.danna@hcl.com>
2021-09-27 19:30:43 +0200

    frmProduct implementation comp-leted

---
+++ userwidgets/com.hcl.toystore.ItemCounter/modules/require/ItemCounterController.js
@@ -0,0 +1,27 @@
+define(function() {
+
+	return {
+		INIT_DONE: false,
+		
+		constructor(baseConfig, layoutConfig, pspConfig) {
+			if(!this.INIT_DONE){
+				this.view.preShow = () => {
+					this.view.flxMinus.onClick = () => {
+						const value = parseInt(this.view.lblCount.text);
+						if(value > 1){
+							this.view.lblCount.text = value - 1;
+						}
+					};
+					this.view.flxPlus.onClick = () => {
+						this.view.lblCount.text = parseInt(this.view.lblCount.text) + 1;
+					};
+				};
+				this.INIT_DONE = true;
+			}
+		},
+		
+		//Logic for getters/setters of custom properties
+		initGettersSetters: function() {
+    }
+	};
+});
--- userwidgets/com.hcl.toystore.Rating/modules/require/RatingController.js
+++ userwidgets/com.hcl.toystore.Rating/modules/require/RatingController.js
@@ -1,20 +1,30 @@
 define(function() {
 
 	return {
-		constructor: function(baseConfig, layoutConfig, pspConfig) {
-
+		constructor(baseConfig, layoutConfig, pspConfig) {
+			
 		},
 		//Logic for getters/setters of custom properties
-		initGettersSetters: function() {
+		initGettersSetters() {
       defineGetter(this, 'rating', () => {
         return this._rating;
       });
       defineSetter(this, 'rating', value => {
-        this._rating = value;
-				if(isNaN(value)){
-					
+				if(isNaN(value) || value < 0 || value > 5){
+					this._rating = 0;
+				} else {
+					this._rating = value;
+					this.setRating();
 				}
       });
-    }
+    },
+		
+		setRating() {
+			const num = Math.ceil(this.rating) - this.rating <= this.rating - Math.floor(this.rating) ? 
+						Math.ceil(this.rating) : Math.floor(this.rating);
+			for(i = 1; i <=5 ; i++) {
+					this.view['lblStarSelected' + i].isVisible = num >= i;
+			} 
+		}
 	};
 });

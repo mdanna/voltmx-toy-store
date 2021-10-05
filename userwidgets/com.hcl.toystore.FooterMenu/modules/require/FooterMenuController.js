7154de6564a35c6e73d308b7ca4334cbf552a8ff
Mario D'Anna <mario.danna@hcl.com>
2021-10-05 17:55:37 +0200

    change to infoplist_configuration.json to make the IR work

--- userwidgets/com.hcl.toystore.FooterMenu/modules/require/FooterMenuController.js
+++ userwidgets/com.hcl.toystore.FooterMenu/modules/require/FooterMenuController.js
@@ -1,10 +1,10 @@
 define(function() {
 	const SKIN_SELECTED = 'lblMenuItemSelected';
 	const SKIN_UNSELECTED = 'lblMenuItemUnselected';
-	
+
 	return {
 		INIT_DONE: false,
-		
+
 		constructor(baseConfig, layoutConfig, pspConfig) {
 			this.view.preShow = () => {
 				this.selectItem(this.itemSelected);
@@ -13,7 +13,7 @@ define(function() {
 						new kony.mvc.Navigation('frmHome').navigate();
 					};
 					this.view.flxMenuCamera.onClick = () => {
-                      	new kony.mvc.Navigation('frmImageRecognitionCamera').navigate();                     
+						new kony.mvc.Navigation('frmImageRecognitionCamera').navigate();                     
 					};
 					this.view.flxMenuSearch.onClick = () => {
 						new kony.mvc.Navigation('frmSearch').navigate();
@@ -28,24 +28,24 @@ define(function() {
 				}
 			};
 		},
-		
+
 		//Logic for getters/setters of custom properties
 		initGettersSetters() {
-      defineGetter(this, 'itemSelected', () => {
-        return this._itemSelected;
-      });
-      defineSetter(this, 'itemSelected', value => {
-        this._itemSelected = value;
-      });
-    },
-		
+			defineGetter(this, 'itemSelected', () => {
+				return this._itemSelected;
+			});
+			defineSetter(this, 'itemSelected', value => {
+				this._itemSelected = value;
+			});
+		},
+
 		selectItem(item){
 			this.view.lblMenuHome.skin = SKIN_UNSELECTED;
 			this.view.lblMenuCamera.skin = SKIN_UNSELECTED;
 			this.view.lblMenuSearch.skin = SKIN_UNSELECTED;
 			this.view.lblMenuLocation.skin = SKIN_UNSELECTED;
 			this.view.lblMenuCart.skin = SKIN_UNSELECTED;
-			
+
 			switch(item){
 				case globals.MENU_ITEM_HOME:
 					this.view.lblMenuHome.skin = SKIN_SELECTED;
@@ -66,7 +66,7 @@ define(function() {
 					break;
 			}
 		},
-		
+
 		onItemSelection(item){
 			//to be overridden 
 		}

db2bfe52e9993c076446c7dee68f87834268aeff
Mario D'Anna <mario.danna@hcl.com>
2021-10-07 14:21:47 +0200

    UI changes for Image Recognition form and removed unused code

---
+++ controllers/mobile/frmImageRecognitionController/frmImageRecognitionController.js
@@ -0,0 +1,78 @@
+define({ 
+
+	imageClassifier: null,
+	
+	onNavigate(){
+		this.view.flxResultBox.setVisibility(false);
+	},
+	
+	onViewCreated(){
+		this.view.init = () => {
+			
+			this.view.flxBack.onClick = () => {
+				new kony.mvc.Navigation(kony.application.getPreviousForm().id).navigate();
+			};
+			
+			var mlCamera = new kony.ui.MLCamera({
+				"height": '100%',
+				"id": "mlCamera",
+				"isVisible": true,
+				"left": "0dp",
+				"top": "0dp",
+				"width": "100%",
+				"zIndex": 1
+			}, {}, {}); 
+			this.view.flxCamera.add(mlCamera);
+
+			this.view.btnRoundFloat.onClick = () => {
+				const irName = this.view.lblObjectType.text;
+				const productName = appData.getProductNameByIRName(irName);
+				if(productName !== null){
+					new kony.mvc.Navigation('frmProduct').navigate(productName);
+				} else {
+					alert('Product not found.');
+				}
+			};
+		};
+		
+		this.view.postShow = () => {
+			this.onFormPostShow();
+		};
+	},
+	
+	onFormPostShow: function(){
+		var config = {};
+		config.modelType = kony.ml.MODEL_TYPE_QUANTIZED;
+		config.onSuccess = this.imageClassificationSuccess;
+		config.onFailure = this.imageClassificationFailure;
+		config.modelPathSource = kony.ml.MODEL_SOURCE_TYPE_BUNDLED;
+		
+		if(kony.os.deviceInfo().name.toLowerCase() === 'iphone'){
+			config["modelInputSize"] = {"width": 299 ,"height": 299};
+			config.modelPath =  "RubikCubeClassifier";
+		} else if (kony.os.deviceInfo().name.toLowerCase() === 'android'){
+			config.modelPath =  "model";
+			config.labelPath = "label";
+		}
+		this.imageClassifier =  new kony.ml.ImageClassifier(config);
+		this.view.mlCamera.setImageClassifier(this.imageClassifier);
+		this.view.mlCamera.startDetection();
+	},
+	
+	imageClassificationSuccess: function(resultArray){
+		if(resultArray !== null && resultArray.length > 0){
+			// resultArray is sorted in descending order ,hence 0th item will be highest confidence
+			this.view.lblObjectType.text = resultArray[0].title; 
+			this.view.lblConfidanceVal.text = (resultArray[0].confidence * 100).toFixed(2) + "%";
+			if(!this.view.flxResultBox.isVisible){
+				this.view.flxResultBox.setVisibility(true);
+			}
+		}
+	},
+	
+	imageClassificationFailure: function(errorCode){
+		alert("errorCode: " + errorCode);
+	},
+	
+	
+});
--- userwidgets/com.hcl.toystore.FooterMenu/modules/require/FooterMenuController.js
+++ userwidgets/com.hcl.toystore.FooterMenu/modules/require/FooterMenuController.js
@@ -13,7 +13,7 @@ define(function() {
 						new kony.mvc.Navigation('frmHome').navigate();
 					};
 					this.view.flxMenuCamera.onClick = () => {
-						new kony.mvc.Navigation('frmImageRecognitionCamera').navigate();                     
+						new kony.mvc.Navigation('frmImageRecognition').navigate();                     
 					};
 					this.view.flxMenuSearch.onClick = () => {
 						new kony.mvc.Navigation('frmSearch').navigate();

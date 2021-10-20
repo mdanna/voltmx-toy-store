148181327e522a85b6ff0a83badad3929fb2f17b
Mario D'Anna <mario.danna@hcl.com>
2021-10-20 11:17:42 +0200

    Fix for the barcode scanner on android

--- controllers/mobile/frmScannerController/frmScannerController.js
+++ controllers/mobile/frmScannerController/frmScannerController.js
@@ -5,12 +5,16 @@ define({
 			if(this.view){
 				this.view.flxBack.onClick = () => {
 					new kony.mvc.Navigation('frmSearch').navigate();
+					//#ifdef iphone
 					kony.application.destroyForm('frmScanner');
+					//#endif
 				};
 
 				this.view.barcodeqrscanner.afterScan = (text) => {
 					new kony.mvc.Navigation('frmSearch').navigate(text);
+					//#ifdef iphone
 					kony.application.destroyForm('frmScanner');
+					//#endif
 				};
 			}
 		};
--- controllers/mobile/frmSearchController/frmSearchController.js
+++ controllers/mobile/frmSearchController/frmSearchController.js
@@ -3,20 +3,20 @@ define({
 	onViewCreated(){
 		this.view.init = () => {
 			this.setCategories();
-			
+
 			this.view.txtSearch.onTouchStart = () => {
 				if(!this.view.txtSearch.text){
 					this.moveUp();
 				}
 			};
-			
+
 			this.view.txtSearch.onTextChange = () => {
 				const text = this.view.txtSearch.text;
 				if(text){
 					this.moveUp();
 				}
 			};
-			
+
 			this.view.txtSearch.onDone = () => {
 				const text = this.view.txtSearch.text;
 				const data = this.view.segRecentKeywords.data || [];
@@ -28,17 +28,17 @@ define({
 					this.search(text);
 				}
 			};
-			
+
 			this.view.flxMicrophone.onTouchEnd = () => {
 				this.view.speechtotext.isVisible = true;
 				this.view.speechtotext.speech();
 			};
-			
+
 			this.view.flxScanner.onClick = () => {
 				new kony.mvc.Navigation('frmScanner').navigate();
 			};
 
-			
+
 			this.view.speechtotext.speechCallback = (text) => {
 				this.view.speechtotext.isVisible = false;
 				this.view.txtSearch.text = text;
@@ -46,23 +46,29 @@ define({
 				this.moveUp();
 				this.search(text);
 			};
-			
+
 			this.view.flxCancel.onTouchEnd = () => {
 				this.view.txtSearch.text = '';
 				this.moveDown();
 			};
-			
+
 			this.view.lblClearAll.onTouchEnd = () => {
 				this.view.segRecentKeywords.setData([]);
 			};
-			
+
 			mEventManager.subscribe(globals.ON_CLICK_KEYWORD, this.onClickRecentKeyword.bind(this));
 			mEventManager.subscribe(globals.ON_CLEAR_KEYWORD, this.onClearRecentKeyword.bind(this));
 		};
 	},
-	
+
 	//this happens on the barcode scanner callback
 	onNavigate(text){
+		//#ifdef android
+		if(kony.application.getPreviousForm().id === 'frmScanner'){
+			kony.application.destroyForm('frmScanner');
+		}
+		//#endif
+
 		if(text){
 			this.view.txtSearch.text = text;
 			this.view.txtSearch.setFocus(true);
@@ -70,7 +76,7 @@ define({
 			this.search(text, /*isRandom*/true);
 		}
 	},
-	
+
 	setCategories(){
 		this.view.flxCategories.removeAll();
 		appData.categories.forEach((category, index) => {
@@ -92,7 +98,7 @@ define({
 			this.view.flxCategories.add(label);
 		});
 	},
-	
+
 	moveUp() {
 		this.view.cmpDefaultHeader.isVisible = false;
 		this.view.flxContent.top = '0';
@@ -104,7 +110,7 @@ define({
 		this.view.flxCategories.isVisible = false;
 		this.view.flxRecentKeywords.isVisible = true;
 	},
-	
+
 	moveDown(){
 		this.view.cmpDefaultHeader.isVisible = true;
 		this.view.flxContent.top = '10%';
@@ -117,7 +123,7 @@ define({
 		this.view.flxRecentKeywords.isVisible = false;
 		this.view.cmpSearchResults.isVisible = false;
 	},
-	
+
 	search(keyword, isRandom){
 		this.view.cmpSearchResults.data = isRandom ? appData.randomSearch() : appData.search(keyword);
 		this.view.cmpSearchResults.isVisible = true;
@@ -126,14 +132,14 @@ define({
 		//the following is necessary to refresh the form and enable the onClick events on search results
 		new kony.mvc.Navigation('frmSearch').navigate();
 	},
-	
+
 	onClickRecentKeyword(){
 		this.view.txtSearch.text = this.view.segRecentKeywords.selectedRowItems[0].lblKeyword;
 		this.view.txtSearch.setFocus(true);
 		this.moveUp();
 		this.search(this.view.txtSearch.text);
 	},
-	
+
 	onClearRecentKeyword(){
 		const index = this.view.segRecentKeywords.selectedIndex[1];
 		this.view.segRecentKeywords.removeAt(index);

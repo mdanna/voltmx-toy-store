c0504641a00c852b58de500bb6f229212df99635
Mario D'Anna <mario.danna@hcl.com>
2021-10-20 12:25:37 +0200

    code clean

--- controllers/mobile/frmSplashController/frmSplashController.js
+++ controllers/mobile/frmSplashController/frmSplashController.js
@@ -1,33 +1,15 @@
 define({ 
-  //#ifdef android
-  onViewCreated(){
-    //Fix for runtime permissions, otherwise, Voice Recognition and barcode scanner/image recognition won't work.
-    kony.application.requestPermissionSet(["android.permission.CAMERA", "android.permission.RECORD_AUDIO"], permissionStatusCallback);
-  }
-  //#endif
+	onViewCreated(){
+		//#ifdef android
+		kony.application.requestPermissionSet(["android.permission.CAMERA", "android.permission.RECORD_AUDIO"], (response) => {
+			for(let key in response) {
+				var result = response[key];
+				if (result === kony.application.PERMISSION_NEVER_ASK_AGAIN) {
+					kony.application.openApplicationSettings();
+				}
+			}
+		});
+		//#endif
+	}
 });
 
-//#ifdef android
-function permissionStatusCallback(response) {
-
-  for (var i in response) {
-
-    /* iterating through permissionSet key value pair from response jsObject where 'i' is permission key and result is permission status */
-    var result = response[i];
-
-    if (result === kony.application.PERMISSION_DENIED) {
-
-      // show message  and raise request again
-
-    } else if (result === kony.application.PERMISSION_NEVER_ASK_AGAIN) {
-
-      // show message and open settings page
-
-      kony.application.openApplicationSettings();
-
-    }
-
-  }
-
-}
-//#endif

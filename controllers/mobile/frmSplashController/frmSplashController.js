dd6da1a1c129390455a2453d32589d65af05d8c7
Iqbal <iqbal.hajat@hcl.com>
2021-10-20 11:01:47 +0100

    Fix for runtime permissions, otherwise, Voice Recognition and barcode scanner/image recognition won't work. Code was taken from recommendations from basecamp.

--- controllers/mobile/frmSplashController/frmSplashController.js
+++ controllers/mobile/frmSplashController/frmSplashController.js
@@ -1,2 +1,33 @@
 define({ 
+  //#ifdef android
+  onViewCreated(){
+    //Fix for runtime permissions, otherwise, Voice Recognition and barcode scanner/image recognition won't work.
+    kony.application.requestPermissionSet(["android.permission.CAMERA", "android.permission.RECORD_AUDIO"], permissionStatusCallback);
+  }
+  //#endif
 });
+
+//#ifdef android
+function permissionStatusCallback(response) {
+
+  for (var i in response) {
+
+    /* iterating through permissionSet key value pair from response jsObject where 'i' is permission key and result is permission status */
+    var result = response[i];
+
+    if (result == kony.application.PERMISSION_DENIED) {
+
+      // show message  and raise request again
+
+    } else if (result == kony.application.PERMISSION_NEVER_ASK_AGAIN) {
+
+      // show message and open settings page
+
+      kony.application.openApplicationSettings();
+
+    }
+
+  }
+
+}
+//#endif
